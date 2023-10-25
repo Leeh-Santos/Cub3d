83dd3926203705761f41c2873d30d58589b65b2f
Le Santos <leandrotyr@gmail.com>
2023-10-25 22:29:22 +0100

    get vars added

--- libft/libft.c
+++ libft/libft.c
@@ -1,52 +1,14 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   cubed.h                                            :+:      :+:    :+:   */
+/*   libft.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: learodri@student.42.fr <learodri>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/23 18:42:08 by learodri          #+#    #+#             */
-/*   Updated: 2023/10/25 20:21:29 by learodri@st      ###   ########.fr       */
+/*   Created: 2023/10/25 20:17:07 by learodri@st       #+#    #+#             */
+/*   Updated: 2023/10/25 20:21:03 by learodri@st      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# ifndef CUB3D_H
-# define CUB3D_H
+# include "../cubed.h"
 
-# include <stdio.h>
-# include <stdlib.h>
-# include <math.h>
-# include <sys/time.h>
-# include <fcntl.h>
-# include <unistd.h>
-# include "get_next.h"
-
-typedef enum e_vars
-{
-   NO,
-   SO,
-   EA,
-   WE,
-} t_vars;
-
-typedef enum e_type
-{
-	r,
-	g,
-	b
-} t_type;
-
-
-
-typedef struct s_cu
-{
-    char **map;
-	char *vars[4];
-	int	f[4];
-	int c[4];
-}t_cu;
-
-void	boom(char *str);
-
-
-# endif
--- main.c
+++ main.c
@@ -6,11 +6,157 @@
 /*   By: learodri@student.42.fr <learodri>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/23 18:38:20 by learodri          #+#    #+#             */
-/*   Updated: 2023/10/24 15:29:07 by learodri@st      ###   ########.fr       */
+/*   Updated: 2023/10/25 20:56:16 by learodri@st      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "cubed.h"
+
+t_cu *cu(void)
+{
+    static t_cu cu;
+
+    return (&cu);
+}
+
+int map_started(char *str)
+{
+	int	i;
+
+	i = 0;
+	//printf("%s\n", str);
+	//printf("%c\n", str[i]);
+	if ((str[0] != 'E') && (str[0] != 'S') && (str[0] != 'N') && (str[0] != 'W') && (str[0] != ' ') && (str[0] != 'F') && (str[0] != 'C') && (str[0] != '\n'))
+	{
+		printf("\n\n parou aqui first %c \n", str[i]);
+		return (1);
+	}
+	if (str[0] == ' ')
+	{
+		i++;
+		while (str[i])
+		{
+			if ((str[i] != ' ') && (str[i] != '\n'))
+			{
+				printf("\n\n parou aqui %c \n", str[i]);
+				return (1);
+			}
+			i++;
+		}
+	}
+	return (0);
+}
+
+void	init_vars()
+{
+ 	cu()->vars[0] = NULL;
+	cu()->vars[1] = NULL;
+	cu()->vars[2] = NULL;
+	cu()->vars[3] = NULL;
+	cu()->c[0] = -1;
+	cu()->c[1] = 0;
+	cu()->c[2] = 0;
+	cu()->c[3] = 0;
+	cu()->f[0] = -1;
+	cu()->f[1] = 0;
+	cu()->f[2] = 0;
+	cu()->f[3] = 0;
+}
+
+char	*subs(int start, char *str)
+{
+	int	i;
+	char	*newstr;
+	
+	i = 0;
+	newstr = malloc(sizeof(char) * ft_strlen(str) - start);
+	if (!newstr)
+		return (NULL);
+	while (str[start])
+	{
+		newstr[i] = str[start];
+		start++;
+		i++;
+	}
+	newstr[i] = '\0';
+	return (newstr);		
+}
+
+void		extract_var(char *str, int var)
+{
+	int i;
+	
+	if (cu()->vars[var])
+		boom("ja tem var"); // com free depois para vars ja alocadas NO WE e tambem o cu->mapa
+	i = 3;
+	while (str[i] && str[i] == ' ')
+		i++;
+	cu()->vars[var] = subs(i, str);
+	
+}
+
+void	extract_rgb(int f_c, char *str)
+{
+	int i;
+	
+	if (f_c == 1)
+	{
+		if (cu()->c[0] != -1)
+			boom("ja tem var c ou f");
+	}
+	if (f_c == 2)
+	{
+		if (cu()->f[0] != -1)
+			boom("ja tem var c ou f");
+	}
+	i = 2;
+	while (str[i] && str[i] == ' ')
+		i++;
+}
+
+void	get_var(char *str)
+{
+	if ((str[0] == 'N') && (str[1] == 'O') && (str[2] == ' '))
+		return (extract_var(str, NO));
+	if ((str[0] == 'S') && (str[1] == 'O') && (str[2] == ' '))
+		return (extract_var(str, SO));
+	if ((str[0] == 'E') && (str[1] == 'A') && (str[2] == ' '))
+		return (extract_var(str, EA));
+	if ((str[0] == 'W') && (str[1] == 'E') && (str[2] == ' '))
+		return (extract_var(str, WE));
+	if ((str[0] == 'F') && (str[1] == ' '))
+		return(extract_rgb(1, str));
+	if ((str[0] == 'C') && (str[1] == ' '))
+		return(extract_rgb(2, str));
+}
+
+void 	print_var()
+{
+	printf("\n");
+	if (cu()->vars[0])
+		printf("no - %s\n", cu()->vars[0]);
+	if (cu()->vars[1])
+		printf("so - %s\n", cu()->vars[1]);
+	if (cu()->vars[2])
+		printf("ea - %s\n", cu()->vars[2]);
+	if (cu()->vars[3])
+		printf("we - %s\n", cu()->vars[3]);
+}
+
+void	search_vars(char **mtx)
+{
+	int i;
+	
+	i = 0;
+	init_vars();
+	while (mtx[i] && (!map_started(mtx[i])))
+	{
+		get_var(mtx[i]);
+		i++;
+	}	
+	print_var();
+	//check_vars
+}
 
 void	print_file(char **mtx)
 {
@@ -29,12 +175,8 @@ void	print_file(char **mtx)
 	}
 }
 
-t_cu *cu(void)
-{
-    static t_cu cu;
 
-    return (&cu);
-}
+
 
 char	**extract_file(char **map, int fd, int counter)
 {
@@ -74,6 +216,8 @@ void check_4_parse(char *cufile)
 	cu()->map = extract_file(NULL, fd, 0);
 	if (!cu()->map)
 		boom("mapa vaziozao\n");
+	close(fd);
+	search_vars(cu()->map);
 	print_file(cu()->map);
 }
 
@@ -90,8 +234,6 @@ int	main(int argc, char *argv[])
 		boom("deu ruim nos args");
 	check_4_parse(argv[1]);
 	
-	
-		
 
 	return 0;
 }
